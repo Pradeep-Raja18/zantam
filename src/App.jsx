50360558827cd9530bc9a9df663cde285942013d
pradeep raja <pradeepsmash11082@gmail.com>
2025-07-30 14:53:33 +0530

    router add

--- src/App.jsx
+++ src/App.jsx
@@ -11,14 +11,14 @@ function App() {
  <>
  <Router basename="/zantam">
       <nav>
-        <NavLink to="/">Home</NavLink> |{" "}
+        <NavLink to="/zantam">Home</NavLink> |{" "}
         <NavLink to="/about">About</NavLink> |{" "}
         <NavLink to="/contact">Contact</NavLink> |{" "}
         <NavLink to="/product">Blog</NavLink>
       </nav>
 
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/zantam" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/product" element={<Products />} />
         <Route path="/contact" element={<Contact />} />
